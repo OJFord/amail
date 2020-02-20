19ead72041c1967f9383303cc3e5365a03f6425f
Ollie Ford <dev@ojford.com>
2020-02-20 22:28:06 +0000

    Connect SES rx -> Lambda relay

--- main.tf
+++ main.tf
@@ -142,4 +142,134 @@ resource "aws_ses_receipt_rule" "eml_store" {
     bucket_name = aws_s3_bucket_policy.eml_store.bucket
     position    = 1
   }
+
+  lambda_action {
+    function_arn    = aws_lambda_function.smtp_relay.arn
+    invocation_type = "Event"
+    position        = 2
+  }
+}
+
+resource "null_resource" "smtp_relay" {
+  triggers = {
+    # TODO: not trigger always, download from GitHub releases?
+    always = uuid() # always trigger and let cargo decide if anything to do
+  }
+
+  provisioner "local-exec" {
+    command     = "docker-compose run lambda-smtp-relay-build"
+    working_dir = path.module
+  }
+
+  provisioner "local-exec" {
+    command     = "cp target/x86_64-unknown-linux-musl/release/handler bootstrap"
+    working_dir = "${path.module}/lambda-smtp-relay"
+  }
+}
+
+data "archive_file" "smtp_relay" {
+  depends_on = [
+    null_resource.smtp_relay,
+  ]
+
+  type        = "zip"
+  source_file = "${path.module}/lambda-smtp-relay/bootstrap"
+  output_path = "${path.module}/lambda-smtp-relay/handler.zip"
+}
+
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "smtp_relay" {
+  name               = "smtp-relay"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+resource "aws_lambda_function" "smtp_relay" {
+  function_name    = "smtp-relay"
+  filename         = data.archive_file.smtp_relay.output_path
+  source_code_hash = data.archive_file.smtp_relay.output_base64sha256
+  runtime          = "provided"
+  handler          = "main"
+  role             = aws_iam_role.smtp_relay.arn
+
+  kms_key_arn = var.smtp_relay.kms_key.arn
+  environment {
+    variables = {
+      S3_BUCKET           = aws_s3_bucket.eml_store.id
+      S3_REGION           = aws_s3_bucket.eml_store.region
+      RELAY_ENVELOPE_FROM = var.smtp_relay.envelope_from
+      RELAY_ENVELOPE_TO   = var.smtp_relay.envelope_to
+      SMTP_HOST           = var.smtp_relay.smtp_host
+      SMTP_USER           = var.smtp_relay.smtp_user
+      SMTP_PASS           = var.smtp_relay.smtp_password
+    }
+  }
+}
+
+resource "aws_lambda_permission" "eml_store" {
+  statement_id   = "smtp-relay"
+  action         = "lambda:InvokeFunction"
+  function_name  = aws_lambda_function.smtp_relay.function_name
+  principal      = "ses.amazonaws.com"
+  source_account = data.aws_caller_identity.current.account_id
+}
+
+data "aws_iam_policy_document" "log_to_cloudwatch" {
+  statement {
+    sid    = "AllowLogging"
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+    resources = [
+      "*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "log_to_cloudwatch" {
+  name   = join("", data.aws_iam_policy_document.log_to_cloudwatch.statement.*.sid)
+  policy = data.aws_iam_policy_document.log_to_cloudwatch.json
+}
+
+resource "aws_iam_role_policy_attachment" "smtp_relay_cloudwatch" {
+  role       = aws_iam_role.smtp_relay.name
+  policy_arn = aws_iam_policy.log_to_cloudwatch.arn
+}
+
+data "aws_iam_policy_document" "eml_fetch" {
+  statement {
+    sid    = "AllowFetchEmail${random_id.eml_store.hex}"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+    ]
+    resources = [
+      "${aws_s3_bucket.eml_store.arn}/*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "eml_fetch" {
+  name   = join("", data.aws_iam_policy_document.eml_fetch.statement.*.sid)
+  policy = data.aws_iam_policy_document.eml_fetch.json
+}
+
+resource "aws_iam_role_policy_attachment" "smtp_relay_eml_fetch" {
+  role       = aws_iam_role.smtp_relay.name
+  policy_arn = aws_iam_policy.eml_fetch.arn
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,28 @@ variable "domains" {
   description = "Domains to verify and use"
   type        = list(string)
 }
+
+variable "smtp_relay" {
+  description = "SMTP relay options"
+  type = object({
+    kms_key = object({
+      arn = string
+    })
+    envelope_from = string
+    envelope_to   = string
+    smtp_host     = string
+    smtp_user     = string
+    smtp_password = string
+  })
+
+  default = {
+    kms_key = {
+      arn = ""
+    }
+    envelope_from = ""
+    envelope_to   = ""
+    smtp_host     = ""
+    smtp_user     = ""
+    smtp_password = ""
+  }
+}
