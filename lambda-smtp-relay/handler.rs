c602f4d36c1bb9df4da1744c6b2b4f78718613d0
Oliver Ford <dev@ojford.com>
2021-03-09 16:58:06 +0000

    Pin lettre to first release with my TLS patch

--- lambda-smtp-relay/handler.rs
+++ lambda-smtp-relay/handler.rs
@@ -1,28 +1,17 @@
-extern crate aws_lambda_events;
-extern crate futures;
-extern crate lambda_runtime;
-extern crate lettre;
 #[macro_use]
 extern crate log;
-extern crate rusoto_core;
-extern crate rusoto_s3;
-extern crate rustls;
-extern crate serde;
-extern crate serde_derive;
-extern crate stderrlog;
-extern crate tokio;
-extern crate webpki_roots;
 
-use lettre::smtp;
-use lettre::Transport;
 use std::boxed::Box;
 use std::env;
 use std::error::Error;
+use std::str::FromStr;
 
 use aws_lambda_events::event::ses::SimpleEmailEvent;
 use lambda_runtime::error::HandlerError;
 use lambda_runtime::lambda;
 use lambda_runtime::Context;
+use lettre::transport::smtp;
+use lettre::Transport;
 use rusoto_core::Region;
 use rusoto_s3::GetObjectRequest;
 use rusoto_s3::S3Client;
@@ -53,19 +42,13 @@ fn handler(e: SimpleEmailEvent, _: Context) -> Result<Output, HandlerError> {
     let s3 = S3Client::new(region);
 
     let smtp_host = env::var("SMTP_HOST").expect("Missing $SMTP_HOST");
-    let mut tls = rustls::ClientConfig::new();
-    tls.root_store
-        .add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
-    let security = smtp::ClientSecurity::Wrapper(smtp::client::net::ClientTlsParameters::new(
-        smtp_host.clone(),
-        tls,
-    ));
-    let mut smtp = smtp::SmtpClient::new((smtp_host.as_ref(), 465 as u16), security)
+    let mut smtp = smtp::SmtpTransport::relay(&smtp_host)
         .expect("Failed to create SMTP client")
         .credentials(smtp::authentication::Credentials::new(
             env::var("SMTP_USER").expect("Missing $SMTP_USER"),
             env::var("SMTP_PASS").expect("Missing $SMTP_PASS"),
-        ));
+        ))
+        .build();
 
     let mail_event = &e.records[0].ses.mail;
     let message_id = mail_event
@@ -80,7 +63,7 @@ fn handler(e: SimpleEmailEvent, _: Context) -> Result<Output, HandlerError> {
 }
 
 #[tokio::main]
-async fn relay_eml(smtp: &mut smtp::SmtpClient, s3: &S3Client, message_id: &str) {
+async fn relay_eml(smtp: &mut smtp::SmtpTransport, s3: &S3Client, message_id: &str) {
     let mut content = Vec::new();
     s3.get_object(GetObjectRequest {
         bucket: env::var("S3_BUCKET").expect("Missing $S3_BUCKET"),
@@ -88,34 +71,27 @@ async fn relay_eml(smtp: &mut smtp::SmtpClient, s3: &S3Client, message_id: &str)
         ..Default::default()
     })
     .await
-    .expect("Failed to retrieve email")
+    .expect(&format!("Failed to retrieve {}", message_id))
     .body
     .unwrap()
     .into_async_read()
     .read_to_end(&mut content)
     .await
-    .expect("Failed to read email");
+    .expect(&format!("Failed to read {}", message_id));
 
-    let email = lettre::Email::new(
-        lettre::Envelope::new(
-            Some(
-                lettre::EmailAddress::new(
-                    env::var("RELAY_ENVELOPE_FROM").expect("Missing $RELAY_ENVELOPE_FROM"),
-                )
-                .unwrap(),
-            ),
-            vec![lettre::EmailAddress::new(
-                env::var("RELAY_ENVELOPE_TO").expect("Missing $RELAY_ENVELOPE_TO"),
-            )
-            .unwrap()],
-        )
-        .unwrap(),
-        message_id.into(),
-        content,
-    );
+    let relay_from = lettre::Address::from_str(
+        &env::var("RELAY_ENVELOPE_FROM").expect("Missing $RELAY_ENVELOPE_FROM"),
+    )
+    .expect("Malformed $RELAY_ENVELOPE_FROM");
 
-    smtp.clone()
-        .transport()
-        .send(email)
-        .expect("Failed to send email");
+    let relay_to = lettre::Address::from_str(
+        &env::var("RELAY_ENVELOPE_TO").expect("Missing $RELAY_ENVELOPE_TO"),
+    )
+    .expect("Malformed $RELAY_ENVELOPE_FROM");
+
+    let envelope =
+        lettre::Envelope::new(Some(relay_from), vec![relay_to]).expect("Failed to build envelope");
+
+    smtp.send_raw(&envelope, &content)
+        .expect(&format!("Failed to send {}", message_id));
 }
