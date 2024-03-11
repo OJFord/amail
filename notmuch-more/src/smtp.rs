ddb80f636941c8a386d86a6235310b9262d905cf
Oliver Ford <dev@ojford.com>
2024-03-11 23:57:50 +0000

    Fix err borrows expr implements reqd traits

--- notmuch-more/src/smtp.rs
+++ notmuch-more/src/smtp.rs
@@ -48,7 +48,7 @@ impl Smtp {
                 println!("[TRACE] Sent message written to {}", file.path().display());
 
                 let index_opts = db.default_indexopts()?;
-                let message = db.index_file(&file.path().as_os_str(), None)?;
+                let message = db.index_file(file.path().as_os_str(), None)?;
                 println!("[TRACE] Sent message indexed as {}", message.id());
 
                 message.add_tag("sent")?;
