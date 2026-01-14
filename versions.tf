ed6267dd577867eb81f2e5f9a36458d4403345bd
Oliver Ford <dev@ojford.com>
2026-01-14 21:41:53 +0000

    Specify relaxed provider versions
    Not pinning exactly, since this is intended to be used as a module.

--- versions.tf
+++ versions.tf
@@ -1,14 +1,24 @@
 terraform {
   required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.4.0"
+    }
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 4.26.0"
+      version = ">= 4.26.0, < 5.0.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.2.2"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
     }
   }
   required_version = "~> 1.7"
